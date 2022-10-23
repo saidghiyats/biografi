ebad81f57c63f239d86af1ff22178123b5275e79
Said Al-Ghiyats <said.ghiyats@gmail.com>
2022-10-23 13:37:54 +0700

    hhh

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,10 @@
 import '../styles/globals.css';
 import { ThemeProvider } from 'next-themes';
-import { ContextProvider } from '../context/Context';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider enableSystem={true} defaultTheme="system" attribute="class">
-      <ContextProvider>
-        <Component {...pageProps} />
-      </ContextProvider>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
