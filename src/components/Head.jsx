bc5a2c1338093e718295043b4663cd5a4180bf5d
saidghiyats <109065973+saidghiyats@users.noreply.github.com>
2022-10-25 10:03:42 +0700

    Update Head.jsx

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Head() {
   return (
     <header className="py-16 flex justify-center">
-      <h1 className="mb-4 text-3xl font-extrabold tracking-tight text-slate-900 dark:text-slate-200 sm:text-4xl">
+      <h1 className="mb-4 font-mono text-3xl font-extrabold tracking-tight text-slate-900 dark:text-slate-200 sm:text-4xl">
         Said Al-Ghiyats
       </h1>
     </header>
