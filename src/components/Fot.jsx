466f37cf67fd2e7583302d11db63108632a91c9f
saidghiyats <109065973+saidghiyats@users.noreply.github.com>
2022-10-25 09:17:10 +0700

    Update Fot.jsx

--- src/components/Fot.jsx
+++ src/components/Fot.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Fot() {
   return (
-    <div className="dark:border-slate-200/5jus tify-center mx-auto mt-12 flex max-w-[52rem]  border-t border-slate-200 pb-10 pt-10 text-sm leading-6 text-slate-500">
+    <div className="dark:border-slate-200/5 jus tify-center mx-auto mt-12 flex max-w-[52rem]  border-t border-slate-200 pb-10 pt-10 text-sm leading-6 text-slate-500">
       <p>Copyright © 2022 Said Al-Ghiyats.</p>
     </div>
   );
