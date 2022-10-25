278330416ee4871a3f3ace7d189d4ff706327392
saidghiyats <109065973+saidghiyats@users.noreply.github.com>
2022-10-25 09:14:58 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,39 @@
 export default function index() {
   return (
     <div className=" max-w-[52rem]  lg:mx-auto">
-      <p className="mt-4 space-y-6 text-base">
+      <p className="mt-4 space-y-6 text-base indent-5">
         Namaku Said Al-Ghiyats, orang biasa memanggilku Said. Aku adalah siswa
         kelas 12 di SMAN 1 Dolok Batu Nanggar. Dunia komputer sangat menarik
         bagiku, inilah yang menjadi hobiku hingga saat ini.
       </p>
-      <p className="mt-4 space-y-6 text-base">
+      <p className="mt-4 space-y-6 text-base indent-5">
         Ayahku adalah seorang guru di salah satu sekolah di Serbelawan sementara
         ibuku bekerja di rumah mengasuh adik-adikku. Kami tinggal di salah satu
         perumahan swasta di Dolok Merangir.
       </p>
-      <p className="mt-4 space-y-6 text-base">
+      <p className="mt-4 space-y-6 text-base indent-5">
         Sedari kecil, Aku selalu dinasehati oleh ibuku untuk rajin beribadah,
         menjadi anak yang sholeh, patuh kepada orang tua, dan pekerja keras.
       </p>
-      <p className="mt-4 space-y-6 text-base">
+      <p className="mt-4 space-y-6 text-base indent-5">
         Riwayat pendidikanku dimulai ketika Aku mulai memasuki dunia Sekolah
         Dasar saat masih berusia 7 tahun kurang. Saat itu Aku masih tinggal
         dirumah lamaku. Sejak SD, Aku selalu meraih peringkat 3 besar di kelas.
         Sejak kecil hingga sekarang aku suka membaca Al-Quran, dan aku juga
         sering mengikuti perlombaan yang rutin dilaksanakan setiap tahunnya.
       </p>
-      <p className="mt-4 space-y-6 text-base">
+      <p className="mt-4 space-y-6 text-base indent-5">
         Pada saat Aku menduduki bangku kelas 6 SD, orangtua ku harus pindah
         kerja sehingga Kami pun juga harus pindah rumah. Aku memutuskan untuk
         pindah sekolah, yang berjarak lebih dekat dari rumah baruku.
       </p>
-      <p className="mt-4 space-y-6 text-base">
+      <p className="mt-4 space-y-6 text-base indent-5">
         Setelah lulus, Aku melanjutkan sekolahku di SMPN Dolok Batu Nanggar.
         Saat menduduki bangku SMP ketertarikanku terhadap komputer dan teknologi
         mulai muncul. Saat itu Aku sering pergi ke warnet untuk belajar dunia
         komputer yang menurutku sangat menarik pada saat itu.
       </p>
-      <p className="mt-4 space-y-6 text-base">
+      <p className="mt-4 space-y-6 text-base indent-5">
         Setelah lulus dari SMP, Aku melanjutkan pendidikanku ke tingkat SMAN 1
         Dolok Batu Nanggar, sekolah saat ini. Aku memutuskan memasuki SMA karena
         Aku ingin melanjutkan pendidikanku ke tingkat Perguruan Tinggi. Ayahku
